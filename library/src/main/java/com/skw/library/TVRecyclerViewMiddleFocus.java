9501c331cf9a8b18ceba80749481d3ce8a8bf446
weishukai <weishukai@le.com>
2018-07-21 21:44:54 +0800

    header focus

--- library/src/main/java/com/skw/library/TVRecyclerViewMiddleFocus.java
+++ library/src/main/java/com/skw/library/TVRecyclerViewMiddleFocus.java
@@ -3,7 +3,10 @@ package com.skw.library;
 import android.content.Context;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -13,40 +16,69 @@ import android.view.View;
  */
 public class TVRecyclerViewMiddleFocus extends TVRecyclerView {
 
-	public TVRecyclerViewMiddleFocus(Context context) {
-		super(context);
-	}
+    public TVRecyclerViewMiddleFocus(Context context) {
+        super(context);
+    }
 
-	public TVRecyclerViewMiddleFocus(Context context, @Nullable AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public TVRecyclerViewMiddleFocus(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public TVRecyclerViewMiddleFocus(Context context, @Nullable AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
+    public TVRecyclerViewMiddleFocus(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 
-	@Override public boolean requestChildRectangleOnScreen(View child, Rect rect, boolean immediate) {
-		// return super.requestChildRectangleOnScreen(child, rect, immediate);
-		focusItemOffsetX = focusItemOffsetY = 0;
-		View focusView = child.findFocus();
-		Rect rectFocus = new Rect();
-		focusView.getDrawingRect(rectFocus);
-		offsetDescendantRectToMyCoords(focusView, rectFocus);
-		int dx = 0, dy = 0;
-		if (getLayoutManager().canScrollHorizontally()) {
-			dx = rectFocus.left - (getWidth() - rectFocus.width()) / 2;
-		} else if (getLayoutManager().canScrollVertically()) {
-			dy = rectFocus.top - (getHeight() - rectFocus.height() / 2);
-		}
-		if (immediate) {
-			scrollBy(dx, dy);
-		} else {
-			smoothScrollBy(dx, dy);
-		}
-		return true;
-	}
+    @Override
+    public boolean requestChildRectangleOnScreen(View child, Rect rect, boolean immediate) {
+        focusItemOffsetX = focusItemOffsetY = 0;
+        View focusView = child.findFocus();
+        Rect rectFocus = new Rect();
+        focusView.getDrawingRect(rectFocus);
+        offsetDescendantRectToMyCoords(focusView, rectFocus);
+        Log.e("middle", rectFocus.toString());
+        Log.e("middle", "" + getWidth() / 2);
+        Log.e("middle", "" + getHeight() / 2);
 
-	@Override public void destory() {
-		super.destory();
-	}
+        LayoutManager layoutManager = getLayoutManager();
+        if (layoutManager != null) {
+            if (layoutManager instanceof LinearLayoutManager) {
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) layoutManager;
+                if (layoutManager.canScrollHorizontally()) {
+                    if ((linearLayoutManager.findFirstVisibleItemPosition() == 0 && rectFocus.left < getWidth() / 2)
+                            || (linearLayoutManager.findLastCompletelyVisibleItemPosition() == (linearLayoutManager.getItemCount() - 1) && (rectFocus.right - rectFocus.width() / 2) >= getWidth() / 2)
+                            || (linearLayoutManager.findLastVisibleItemPosition() == (linearLayoutManager.getItemCount() - 1) && rectFocus.left > getWidth() / 2)) {
+                        return super.requestChildRectangleOnScreen(child, rect, immediate);
+                    }
+                } else if (layoutManager.canScrollVertically()) {
+                    if ((linearLayoutManager.findFirstVisibleItemPosition() == 0 && rectFocus.top < getHeight() / 2)
+                            || (linearLayoutManager.findLastCompletelyVisibleItemPosition() == (linearLayoutManager.getItemCount() - 1) && (rectFocus.bottom - rectFocus.width() / 2) >= getHeight() / 2)
+                            || (linearLayoutManager.findLastVisibleItemPosition() == (linearLayoutManager.getItemCount() - 1) && rectFocus.top > getHeight() / 2)) {
+                        return super.requestChildRectangleOnScreen(child, rect, immediate);
+                    }
+                }
+
+            } else if (layoutManager instanceof StaggeredGridLayoutManager) {
+                StaggeredGridLayoutManager staggeredGridLayoutManager = (StaggeredGridLayoutManager) layoutManager;
+                // TODO: 17/1/4
+            }
+        }
+
+        int dx = 0, dy = 0;
+        if (getLayoutManager().canScrollHorizontally()) {
+            dx = rectFocus.left - (getWidth() - rectFocus.width()) / 2;
+        } else if (getLayoutManager().canScrollVertically()) {
+            dy = rectFocus.top - (getHeight() - rectFocus.height() / 2);
+        }
+        if (immediate) {
+            scrollBy(dx, dy);
+        } else {
+            smoothScrollBy(dx, dy);
+        }
+        return true;
+    }
+
+    @Override
+    public void destory() {
+        super.destory();
+    }
 }
